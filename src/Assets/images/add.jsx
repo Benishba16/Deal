ec3de63e98363fee24b65ee07e795a412832a120
Benishba16 <92793953+Benishba16@users.noreply.github.com>
2022-05-30 18:25:51 +0530

    uncomment svg

--- src/Assets/images/add.jsx
+++ src/Assets/images/add.jsx
@@ -2,15 +2,6 @@ import React from "react";
 
 function Add(width, height, color = "white") {
   return (
-    // <svg
-    //   xmlns="http://www.w3.org/2000/svg"
-    //   width={width}
-    //   height={height}
-    //   viewBox="0 0 32 32"
-    //   style={{fill: `${color}`}}
-    // >
-    //   <path d="M17 6.667a1 1 0 00-2 0V15H6.667a1 1 0 000 2H15v8.333a1 1 0 002 0V17h8.333a1 1 0 000-2H17V6.667z"></path>
-    // </svg>
     <svg
       width='14'
       height='14'
